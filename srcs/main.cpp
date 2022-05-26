0c358a514f769bb8409ce8c740712ab898a460d0
Frederico Meira <fmeira@c1r13s6.42lisboa.com>
2022-05-27 00:14:43 +0100

    starting containers

---
+++ srcs/main.cpp
@@ -0,0 +1,11 @@
+//***************************//
+//*Template by pulgamecanica*//
+//***************************//
+
+#include "ft_containers.hpp"
+
+int	main(void)
+{
+	std::cout << "Hello Friend\nPulgamecanica greets you :D\n";
+	return (0);
+}
