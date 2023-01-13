22bd837bb4b473652a484263b984aa234b956da0
coderMeira <fredericomeiraa@gmail.com>
2023-01-13 23:57:16 +0000

    learning how to delete nodes from a BST

--- bst_tests/inc/bst.hpp
+++ bst_tests/inc/bst.hpp
@@ -14,7 +14,8 @@ class BST {
   Node<T> *root_;
 
   // Function to create a new node
-  Node<T>* createNode(T data) {
+  Node<T>* createNode(T data)
+  {
     Node<T> *newNode = new Node<T>;
     newNode->data = data;
     newNode->left = NULL;
@@ -23,7 +24,8 @@ class BST {
   }
 
   // Function to insert a new node in the binary search tree
-  void insert(Node<T>* &root, T data) {
+  void insert(Node<T>* &root, T data)
+  {
     if (root == NULL) {
       root = createNode(data);
     } else if (data <= root->data) {
@@ -34,27 +36,79 @@ class BST {
   }
 
   // Function to print the binary search tree in-order (ascending order)
-  void printTree(Node<T>* root) {
+  void printTree(Node<T>* root)
+  {
     if (root == NULL) return;
     printTree(root->left);
     std::cout << root->data << " ";
     printTree(root->right);
   }
 
-  public:
-    BST() {
-      root_ = NULL;
+  bool  searchNode(Node<T>* root, T value)
+  {
+    if (root == NULL)
+      return (false);
+
+    if (value == root->data)
+      return (true);
+
+    else if (value < root->data)
+      return (searchNode(root->left, value));
+
+    else if (value > root->data)
+      return (searchNode(root->right, value));
+
+    return (false);
+  }
+
+  Node<T>* deleteNode(Node<T>* root, T value)
+  {
+    if (root == NULL)
+      return (root);
+    if (value < root->data)
+      root->left = deleteNode(root->left, value);
+    else if (value > root->data)
+      deleteNode(root->right, value);
+
+    if (value == root->data)
+    {
+      if (root->left == NULL && root->right == NULL)
+      {
+        delete (root);
+        return (NULL);
+      }
+      else if (root->left && root->right == NULL)
+      {
+        Node<T>* tmp = root->left;
+        delete (root);
+        return (tmp);
+      }
+      else if (root->right && root->left == NULL)
+      {
+        Node<T>* tmp = root->right;
+        delete (root);
+        return (tmp);
+      }
+      else
+      {
+
+      }
     }
+    return (root);
+  }
+
+  public:
+    BST(){root_ = NULL;}
 
     // Function to insert a new node in the binary search tree
-    void insert(T data) {
-      insert(root_, data);
-    }
+    void insert(T data){insert(root_, data);}
 
     // Function to print the binary search tree in-order (ascending order)
-    void printTree() {
-      printTree(root_);
-    }
+    void printTree(){printTree(root_);}
 
-    Node<T>*  get_root(){return root_;};
+    bool searchNode(T value){return (searchNode(root_, value));}
+
+    void deleteNode(T value){deleteNode(root_, value);};
+
+    Node<T>*  get_root(){return root_;}
 };
--- bst_tests/src/main.cpp
+++ bst_tests/src/main.cpp
@@ -5,15 +5,25 @@
 int main(void)
 {
 	BST<int> tree;
+	int find = 5;
 
-	tree.insert(1);
+	tree.insert(5);
 	tree.insert(2);
-	tree.insert(3);
+	tree.insert(6);
 	tree.insert(4);
+	tree.insert(1);
+	tree.insert(3);
+	tree.insert(11);
 
-	tree.printTree();
-	// const visualizer<int> v(tree);
-	// cout << endl << endl;
-	// v.visualize();
-	// cout << endl << endl;
+
+	// tree.printTree();
+	const visualizer<int> v(tree);
+	cout << endl << endl;
+	v.visualize();
+	cout << endl << endl;
+
+	if (tree.searchNode(find))
+		cout << find << " was found in the tree\n";
+	else
+		cout << find << " was NOT found in the tree\n";
 }
